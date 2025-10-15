2901ae2033726d5f2715b640d638ef79f1c4e6db
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-15 23:24:25 +0200

    Add timeline with callback

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-gsap.from(".container-body", {
-    x: "-1000px",
-    opacity: 0,
-    duration: 1,
-    delay: 0.5,
-    ease: "power2.out"
-});
+// gsap.from(".container-body", {
+//     x: "-1000px",
+//     opacity: 0,
+//     duration: 1,
+//     delay: 0.5,
+//     ease: "power2.out"
+// });
+
+const endAnimation = () => {
+    alert("Animacja zakończona");
+};
+
+let tl = gsap.timeline({onComplete: endAnimation});
+tl
+    .from(".container-body h1", {x: "-1000px", opacity: 0, duration: 1, ease: "power2.out"})
+    .from(".container-body p", {x: "-1000px", opacity: 0, duration: 1, ease: "power2.out"}, "-=0.5");
 
 gsap.fromTo(".page-section.second",{
     x: "-1000px",
