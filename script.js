375a59e6190b24f2738e46ad1cf5850124077e53
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-15 21:03:25 +0200

    First animationa added

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.from(".container-body", {
+    x: "-1000px",
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    ease: "power2.out"
+});
+
+gsap.fromTo(".page-section.second",{
+    x: "-1000px",
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    ease: "power2.out"
+}
+);
